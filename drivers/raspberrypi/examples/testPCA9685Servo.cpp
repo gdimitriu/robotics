020ddf1afd0e2e2d218230e0e06424b116d3110e
gdimitriu <gabriel.dimitriu@yahoo.com>
2020-12-15 00:57:48 -0800

    changed the headers

--- drivers/raspberrypi/examples/testPCA9685Servo.cpp
+++ drivers/raspberrypi/examples/testPCA9685Servo.cpp
@@ -1,23 +1,24 @@
 /*
- Servo test using adafruit PWM Servo Driver based on PCA9585.
- Copyright (C) 2020 Gabriel Dimitriu
- All rights reserved.
+ * Servo test using adafruit PWM Servo Driver based on PCA9585.
+ * Copyright (C) 2020 Gabriel Dimitriu
+ * All rights reserved.
+ * This file is part of Robotics project.
 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
+ * Robotics is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
 
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
+ * Robotics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
 
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Robotics; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 #include <Adafruit_PWMServoDriver.h>
 #include <stdio.h>
 
--- framework/raspberrypi/control_engines/CFactoryEngine.cpp
+++ framework/raspberrypi/control_engines/CFactoryEngine.cpp
@@ -5,19 +5,21 @@
  *      Author: Gabriel Dimitriu
  * Copyright (C) 2020 Gabriel Dimitriu
  * All rights reserved.
+ *
+ * This file is part of Robotics project.
 
- * This library is free software; you can redistribute it and/or
+ * Robotics is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
 
- * This library is distributed in the hope that it will be useful,
+ * Robotics is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
 
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
+ * License along with Robotics; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
