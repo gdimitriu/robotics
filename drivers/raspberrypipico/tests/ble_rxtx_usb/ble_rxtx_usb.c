90173cf9566d4e5a52135a50160a65e4e8b26a15
gdimitriu <gabriel.dimitriu@yahoo.com>
2021-12-13 00:01:20 -0800

    added exit operation

--- drivers/raspberrypipico/tests/ble_rxtx_usb/ble_rxtx_usb.c
+++ drivers/raspberrypipico/tests/ble_rxtx_usb/ble_rxtx_usb.c
@@ -7,30 +7,35 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
 #include <hardware/uart.h>
+#include <string.h>
 
 int main() {
-	 char buffer[256];
-	 int index = 0;
-	 stdio_usb_init();
-	 //initialize UART 1
-	 uart_init(uart1, 38400);
+	char buffer[256];
+	int index = 0;
 
-	 // Set the GPIO pin mux to the UART - 4 is TX, 5 is RX
-	 gpio_set_function(4, GPIO_FUNC_UART);
-	 gpio_set_function(5, GPIO_FUNC_UART);
-	 uart_set_translate_crlf(uart1,1);
+	stdio_usb_init();
+	//initialize UART 1
+	uart_init(uart1, 38400);
 
-	 while (1) {
-		 if (uart_is_readable(uart1)) {
-			 char readed =uart_getc(uart1);
-			 buffer[index] = readed;
-			 if (readed == '#') {
-				 buffer[index+1] = '\0';
-				 printf("%s\n",buffer);
-				 index = 0;
-			 } else {
-				 index++;
-			 }
-		 }
-	 }
+	// Set the GPIO pin mux to the UART - 4 is TX, 5 is RX
+	gpio_set_function(4, GPIO_FUNC_UART);
+	gpio_set_function(5, GPIO_FUNC_UART);
+	uart_set_translate_crlf(uart1, 1);
+
+	while (1) {
+		if (uart_is_readable(uart1)) {
+			char readed = uart_getc(uart1);
+			buffer[index] = readed;
+			if (readed == '#') {
+				buffer[index + 1] = '\0';
+				printf("%s\n", buffer);
+				index = 0;
+				if (strcmp(buffer, "exit#") == 0) {
+					return 1 ;
+				}
+			} else {
+				index++;
+			}
+		}
+	}
 }
