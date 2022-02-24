bda82f4e20dc514aeadb2d97d824a3b8a94b4f76
gdimitriu <gabriel.dimitriu@yahoo.com>
2022-02-24 07:57:20 -0800

    prepare for the new camera from raspberry pi zero impl

--- framework/raspberrypi/settings_crosscompiler_pi.sh
+++ framework/raspberrypi/settings_crosscompiler_pi.sh
@@ -5,9 +5,9 @@ PREFIX_GPIO=/home/onion/raspberrypi/libs/pigpio
 PREFIX_DRIVERS=/home/onion/robotics/drivers/raspberrypi
 export PREFIX_GPIO
 export PREFIX_DRIVERS
-PREFIX_RASPICAM=/home/onion/raspberrypi/libs/raspicam
-export PREFIX_RASPICAM
-PREFIX_VC=/home/onion/raspberrypi/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/arm-bcm2708-linux-gnueabi/sysroot/opt/vc
-export PREFIX_VC 
+#PREFIX_RASPICAM=/home/onion/raspberrypi/libs/raspicam
+#export PREFIX_RASPICAM
+#PREFIX_VC=/home/onion/raspberrypi/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/arm-bcm2708-linux-gnueabi/sysroot/opt/vc
+#export PREFIX_VC 
 PREFIX_WIRINGPI=/home/onion/raspberrypi/libs/wiringpi
 export PREFIX_WIRINGPI
