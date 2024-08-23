445e25c77becfd6d02517b25221235b92b6e7f04
gdimitriu <gabriel.dimitriu@yahoo.com>
2024-08-22 23:39:53 -0700

    added some more links

---
+++ poc_tests/pico/settings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PICO_SDK_PATH=/home/onion/pico-sdk
+export PICO_SDK_PATH
+PICO_GCC_TRIPLE=arm-none-eabi
+export PICO_GCC_TRIPLE
+PICO_SDK_FETCH_FROM_GIT=0
+export PICO_SDK_FETCH_FROM_GIT
+PICO_LIBS_PATH=/home/onion/micro/pico-libs
+export PICO_LIBS_PATH
+
